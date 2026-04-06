18c936cfc920eb2e88cb40bf158f6e56c399cd70
Edward (Eddie) Shiang <47203516+eshiang21@users.noreply.github.com>
2026-04-05 23:59:00 -0400

    Add files via upload

---
+++ site/js/main.js
@@ -0,0 +1,5 @@
+// Active nav link
+const currentPage = location.pathname.split('/').pop() || 'index.html';
+document.querySelectorAll('.nav-links a').forEach(a => {
+  if (a.getAttribute('href') === currentPage) a.classList.add('active');
+});
