a43d0048940a364d4bff93cc677d49a5ff166be3
Edward (Eddie) Shiang <47203516+eshiang21@users.noreply.github.com>
2026-04-06 00:04:24 -0400

    Delete site directory

--- site/js/main.js
+++
@@ -1,5 +0,0 @@
-// Active nav link
-const currentPage = location.pathname.split('/').pop() || 'index.html';
-document.querySelectorAll('.nav-links a').forEach(a => {
-  if (a.getAttribute('href') === currentPage) a.classList.add('active');
-});
